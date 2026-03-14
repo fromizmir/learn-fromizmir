c05b732411287c7bea74ef9fd4b55d02c18e7eca
fromizmir <mahmut.tkolu@gmail.com>
2026-03-14 21:41:23 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
