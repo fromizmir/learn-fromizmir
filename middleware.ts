be4ca6fa80170e1289d8e82576f02ca88c6baa9a
fromizmir <mahmut.tkolu@gmail.com>
2026-03-14 20:36:55 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,2 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-
-export default authMiddleware({
-  // Bu regex, tüm rotaların herkese açık olmasını sağlar.
-  publicRoutes: ["/(.*)"],
-});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// Middleware devre dışı bırakıldı - Clerk kaldırıldı
+export {};
