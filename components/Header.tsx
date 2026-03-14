5f6a410ebf8549bd58fcb5a0eae06a3f136a4938
fromizmir <mahmut.tkolu@gmail.com>
2026-03-14 20:38:50 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,6 @@
-// Dosya: components/Header.tsx
-
 "use client";
-import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
-import styles from './Header.module.css'; // Yeni stil dosyasını import et
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
@@ -15,15 +12,6 @@ export default function Header() {
         <Link href="/quizzes" className={styles.navLink}>
           Quizzes
         </Link>
-      </div>
-      <div className={styles.rightSection}>
-        <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-        <SignedOut>
-          <Link href="/sign-in" className={styles.navLink}>Sign In</Link>
-          <Link href="/sign-up" className={styles.navLink}>Sign Up</Link>
-        </SignedOut>
       </div>
     </header>
   );
